157075b020c2785915311be0123b088fb474e232
devdariill <93483481+devdariill@users.noreply.github.com>
2023-10-17 15:52:14 +0000

    protected route / new URL

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,15 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function middleware () {
-  return NextResponse.json({
-    message: "Hello from the middleware!"
-  })
+const isLoggedIn = false
+
+export function middleware (request: NextRequest) {
+  console.log(new Date,"Hello from the middleware!", request.url)
+  if (!isLoggedIn && request.url === 'https://localhost:3000/profile') {
+    return NextResponse.redirect(new URL('/', request.url))
+    // return NextResponse.redirect('/')
+  }
+  return NextResponse.next()
+  // return NextResponse.json({
+  //   message: "Hello from the middleware!"
+  // })
 }
