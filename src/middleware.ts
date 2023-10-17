80e805c644308c2795b6712ef2d6b92b5c387615
devdariill <93483481+devdariill@users.noreply.github.com>
2023-10-17 15:58:44 +0000

    cookies / new Headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,26 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from 'next/server'
 
 const isLoggedIn = false
 
-export function middleware (request: NextRequest) {
-  console.log(new Date,"Hello from the middleware!", request.url)
-  if (!isLoggedIn && request.url === 'https://localhost:3000/profile') {
-    return NextResponse.redirect(new URL('/', request.url))
-    // return NextResponse.redirect('/')
+export function middleware(request: NextRequest) {
+  let headers = new Headers (request.headers)
+  // let cookie = request.cookies.get('my-cookie')
+  if (!isLoggedIn){
+    return NextResponse.next()
   }
-  return NextResponse.next()
-  // return NextResponse.json({
-  //   message: "Hello from the middleware!"
-  // })
+  return NextResponse.redirect(new URL('/', request.url))
+
+  // console.log(new Date,"Hello from the middleware!", request.url)
+  // if (!isLoggedIn && request.url === 'https://localhost:3000/profile') {
+  //   return NextResponse.redirect(new URL('/', request.url))
+  //   // return NextResponse.redirect('/')
+  // }
+  // return NextResponse.next()
+  // // return NextResponse.json({
+  // //   message: "Hello from the middleware!"
+  // // })
+}
+
+export const config = {
+  matcher : ['/profile'],
 }
